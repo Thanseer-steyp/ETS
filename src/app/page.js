022783f1b480ea15fa0431938840807e2a148c9f
thanseer-steyp <steypthanseer@gmail.com>
2025-07-03 17:22:06 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <header className="bg-[#131d30] w-full flex justify-center">
-        <div className="bg-red-600 w-2/4">Hello</div>
+        <div className="bg-red-600 w-2/4">Helloo</div>
       </header>
     </div>
   );
